3f81a0ff6e2b23c0eeda1422e16e902d3124e042
Oksana Komarnytska <okazimiriv@gmail.com>
2023-05-27 15:32:40 +0200

    styles finish

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
 import FriendListItem from 'components/FriendListItem/FriendListItem';
+import styles from '../FriendList/FriendList.module.css';
 
 const FriendList = ({ friendsList }) => {
   return (
-    <ul class="friend-list">
+    <ul className={styles.friendsList}>
       {friendsList.map(({ id, avatar, name, isOnline }) => (
         <FriendListItem
           key={id.toString()}
--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -1,14 +1,24 @@
 import PropTypes from 'prop-types';
 import defaultAvatar from '../Profile/defavatar.png';
+import styles from '../FriendListItem/FriendListItem.module.css';
 
 const FriendListItem = ({ avatar = defaultAvatar, name, isOnline }) => {
   return (
     // <ul class="friend-list"></ul>
 
-    <li class="item">
-      <span class="status"></span>
-      <img class="avatar" src={avatar} alt="User avatar" width="48" />
-      <p class="name">{name}</p>
+    <li className={styles.item}>
+      {isOnline ? (
+        <span className={styles.online}></span>
+      ) : (
+        <span className={styles.offline}></span>
+      )}
+      <img
+        className={styles.avatar}
+        src={avatar}
+        alt="User avatar"
+        width="48"
+      />
+      <p className={styles.name}>{name}</p>
     </li>
   );
 };
